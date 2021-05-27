0588210bdff4c31ecc87fec2d877fff4b1f0397a
SophiaL1024 <sophiafengliu@gmail.com>
2021-05-27 21:48:52 +0000

    refactor getAllReservations by sql requery

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -74,7 +74,14 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `SELECT * FROM reservations WHERE guest_id=$1  AND CURRENT_DATE NOT BETWEEN start_date AND end_date LIMIT $2;`
+  return pool.query(queryString, [guest_id, limit])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.getAllReservations = getAllReservations;
 
